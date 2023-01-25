d0b04aac80fdfe111d26eeed1f141bb4145ec805
Vladislava Mayorova <vladamayor@yandex.ru>
2023-01-25 14:21:39 +0300

    update

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import psycopg2
 from pprint import pprint
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
-
-with open('settings.txt', 'r') as f:
-    password_db = f.readline()
-
-conn = psycopg2.connect(database='home_work', user='postgres', password=password_db)
-
-cur = conn.cursor()
 
 class User:
 
-    def __init__(self):
+    def __init__(conn):
         pass
 
     def _create_table(self):
@@ -109,18 +105,19 @@ class User:
         pprint(cur.fetchall())
 
 
+
 if __name__ == '__main__':
-    Client = User()
-    Client.add_user('Seva', 'Mayorov', 'seva@')
-    Client.add_number('+79999999999', 1)
-    Client.add_user('Vlada', 'Mayorova', 'vlada@')
-    Client.add_number('+79999999990', 1)
-    Client.add_number('+78888888888', 2)
-    Client.update_user('Sevochka', 'Mayorov', 'seva@', '+79999999999', '+71111111111', 1)
-    Client.delete_number(1, '+79999999990')
-    Client.find_user('Vlada', 'Mayorova', 'vlada@', None)
-    Client.delete_user(2)
-
-
-cur.close()
-conn.close()
+    with psycopg2.connect(database='home_work', user=os.getenv('LOGIN'), password=os.getenv('PASS')) as conn:
+        with conn.cursor() as cur:
+            Client = User()
+            Client.add_user('Seva', 'Mayorov', 'seva@')
+            Client.add_number('+79999999999', 1)
+            Client.add_user('Vlada', 'Mayorova', 'vlada@')
+            Client.add_number('+79999999990', 1)
+            Client.add_number('+78888888888', 2)
+            Client.update_user('Sevochka', 'Mayorov', 'seva@', '+79999999999', '+71111111111', 1)
+            Client.delete_number(1, '+79999999990')
+            Client.find_user('Vlada', 'Mayorova', 'vlada@', None)
+            Client.delete_user(2)
+            Client.show_info()
+    conn.close()
